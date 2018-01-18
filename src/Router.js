75463e19f97463f5ebd58f95eb1da912d722817c
Cristobal Miranda <cristobal.miranda.u@gmail.com>
2018-01-18 17:26:15 -0300

    final commit con nueva vista home

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router, Scene } from 'react-native-router-flux';
 import Welcome from './components/Login/Welcome';
 import ForgotPassword from './components/Login/ForgotPassword';
+import Home from './components/Home';
 
 const RouterComponent = () => {
     return (
@@ -9,6 +10,7 @@ const RouterComponent = () => {
         <Scene key="root" hideNavBar="true">
           <Scene key="Welcome" component={Welcome} title="Welcome" />
           <Scene key="ForgotPassword" component={ForgotPassword} title="ForgotPassword" />
+          <Scene key="Home" component={Home} title="Home" />
         </Scene>
       </Router>
     );
--- src/components/Login/ForgotPassword.js
+++ src/components/Login/ForgotPassword.js
@@ -47,7 +47,7 @@ class ForgotPassword extends Component {
       }
 
       return (
-        <TouchableOpacity style={styles.touchable} onPress={this.onButtonPressRecovery}>
+        <TouchableOpacity style={styles.touchable} onPress={() => this.onButtonPressRecovery()}>
           <LinearGradient
             colors={['#fd7292', '#fd6342']}
             style={styles.gradientButton}
@@ -103,7 +103,7 @@ class ForgotPassword extends Component {
             { this.renderButton() }
             <Body>
               <Text style={styles.errorTextStyle}>
-                {this.props.error}
+                {this.props.error2}
               </Text>
             </Body>
           </Card>
@@ -153,16 +153,17 @@ const styles = StyleSheet.create({
   errorTextStyle: {
     fontSize: 15,
     alignSelf: 'center',
-    color: 'red'
+    color: 'red',
+    marginBottom: 10
   }
 });
 
 const mapStateToPros = ({ auth }) => {
-  const { email, password, error, loading } = auth;
+  const { email, password, error2, loading } = auth;
     return {
       email,
       password,
-      error,
+      error2,
       loading
     };
 };
