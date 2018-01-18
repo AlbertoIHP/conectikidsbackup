d1608db7fb198ce2688e1cbe79b773df221c4a44
Cristobal Miranda <cristobal.miranda.u@gmail.com>
2018-01-18 13:49:59 -0300

    add route for forgot password

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Router, Scene } from 'react-native-router-flux';
-import  Welcome  from './components/Login/Welcome';
+import Welcome from './components/Login/Welcome';
+import ForgotPassword from './components/Login/ForgotPassword';
 
 const RouterComponent = () => {
-    return(
+    return (
       <Router hideNavBar="true">
         <Scene key="root" hideNavBar="true">
-          <Scene key="Welcome" component={Welcome} title="Welcome"  />
+          <Scene key="Welcome" component={Welcome} title="Welcome" />
+          <Scene key="ForgotPassword" component={ForgotPassword} title="ForgotPassword" />
         </Scene>
       </Router>
     );
