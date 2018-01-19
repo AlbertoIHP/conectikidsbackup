4ebc566d26134af56c541c9753085a497e7e0c03
Alberto Ignacio <a.herrera07@ufromail.cl>
2018-01-19 14:25:40 -0300

    pantalla contenedora

--- src/Router.js
+++ src/Router.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Router, Scene } from 'react-native-router-flux';
 import Welcome from './components/Login/Welcome';
 import ForgotPassword from './components/Login/ForgotPassword';
-import Home from './components/Home';
+import MainContainer from './components/MainContainer';
 
 const RouterComponent = () => {
     return (
       <Router hideNavBar="true">
         <Scene key="root" hideNavBar="true">
           <Scene key="Welcome" component={Welcome} title="Welcome" />
+          <Scene key="MainContainer" component={MainContainer} title="MainContainer" />
           <Scene key="ForgotPassword" component={ForgotPassword} title="ForgotPassword" />
-          <Scene key="Home" component={Home} title="Home" />
         </Scene>
       </Router>
     );
