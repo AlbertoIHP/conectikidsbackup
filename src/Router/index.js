31d85e0c331dacafcb53538527e77fb937361341
Alberto Ignacio <a.herrera07@ufromail.cl>
2018-01-22 15:25:01 -0300

    login arreglado

--- src/Router/index.js
+++ src/Router/index.js
@@ -11,8 +11,8 @@ const RouterComponent = () => {
     return (
       <Router hideNavBar="true">
         <Scene key="root" hideNavBar="true">
-          <Scene key="MainContainer" component={MainContainer} title="MainContainer" initial={true}/>
-          <Scene key="Welcome" component={Welcome} title="Welcome"  />
+          <Scene key="MainContainer" component={MainContainer} title="MainContainer"/>
+          <Scene key="Welcome" component={Welcome} title="Welcome"  initial={true}/>
           <Scene key="ForgotPassword" component={ForgotPassword} title="ForgotPassword" />
           <Scene key="AddActivity" component={AddActivity} title="AddActivity" />
           <Scene key="AddTask" component={AddTask} title="AddTask" />
--- src/components/Login/Welcome.js
+++ src/components/Login/Welcome.js
@@ -124,7 +124,8 @@ const styles = StyleSheet.create({
   imageBackground: {
     flex: 1,
     position: 'absolute',
-    height: "100%"
+    height: "100%",
+    width: "100%"
   },
   imageLogo: {
     flex: 1,
