4dcfdb5c2cc7e39a87e18407b458bca585a88294
Cristobal Miranda <cristobal.miranda.u@gmail.com>
2018-01-23 15:45:54 -0300

    se agrega componente post y comentarios

---
+++ src/components/FeedHome/Post/comments/components/comment/index.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+import {TextArea, StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import Image from 'react-native-image-progress';
+
+
+class Comment extends React.Component {
+    constructor(props){
+        super(props);
+        console.log(this.props.info);
+        this.state = {
+            userPhoto: require('../../../img/default-profile.png'),
+        }
+    }
+
+
+    render() {
+        return (
+            <View style={styles.parentContainer}>
+            <View style={styles.container}>
+            <View style={{flex: 1}}>
+            {
+                (this.props.info && this.props.info.photoUrl) &&
+                <Image
+                  style={styles.image}
+                  source={{uri: this.props.info.photoUrl}}
+                />
+            }
+            {
+                (!this.props.info || !this.props.info.photoUrl) &&
+                <Image
+                  style={styles.image}
+                  source={this.state.userPhoto}
+                />
+            }
+            </View>
+            <View style={{flex: 6}}>
+            <Text>
+            <Text style={{fontWeight: 'bold'}}> {this.props.info.displayName}</Text>
+            {`: ${this.props.info.text}`}
+            </Text>
+            </View>
+            </View>
+            <View
+              style={{
+                borderBottomColor: '#a5a5a5',
+                borderBottomWidth: 0.8,
+                height: 20
+              }}
+            />
+            </View>
+        )
+    }
+
+}
+export default Comment
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  parentContainer: {
+    paddingBottom: 10
+  },
+    image: {
+    flex: 1,
+    width: 35,
+    height: 35,
+    resizeMode: 'contain'
+  },
+})
